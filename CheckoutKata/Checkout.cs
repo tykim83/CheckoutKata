40fbb13174810cba4ead2c3cf58d44831f7a725d
tykim83 <agate.fabrizio83@gmail.com>
2022-08-10 00:02:53 +0100

    Add implementation for discounts

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -40,10 +40,41 @@ public class Checkout : ICheckout
 
         foreach (var basketItem in _basket)
         {
-            var item = _itemsList.Single(c => c.SKU == basketItem.SKU);
-            total += basketItem.Quantity * item.UnitPrice;
+            total += CalculateItemPrice(basketItem);
         }
 
         return total;
     }
+
+    private decimal CalculateItemPrice(BasketItem basketItem)
+    {
+        var item = _itemsList.Single(c => c.SKU == basketItem.SKU);
+        var discount = 0.0m;
+
+        if (item.Discount is not null)
+        {
+            discount = item.Discount.DiscountType switch
+            {
+                DiscountType.Fixed => CalculateFlatDiscount(basketItem, item),
+                DiscountType.Percentage => CalculatePercentageDiscount(basketItem, item),
+                _ => 0.0m,
+            };
+        }
+
+        return basketItem.Quantity * item.UnitPrice - discount;
+    }
+
+    private readonly Func<BasketItem, Item, decimal> CalculateFlatDiscount = (basketItem, item) =>
+    {
+        var discountMultiplier = basketItem.Quantity / item.Discount!.UnitRequired;
+        var fullPrice = item.UnitPrice * item.Discount.UnitRequired;
+        return (fullPrice - item.Discount.Value) * discountMultiplier;
+    };
+
+    private readonly Func<BasketItem, Item, decimal> CalculatePercentageDiscount = (basketItem, item) =>
+    {
+        var discountMultiplier = basketItem.Quantity / item.Discount!.UnitRequired;
+        var fullPrice = item.UnitPrice * item.Discount.UnitRequired;
+        return (fullPrice * item.Discount.Value / 100) * discountMultiplier;
+    };
 }
